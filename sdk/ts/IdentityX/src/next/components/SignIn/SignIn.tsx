32b497dbd1ab51a842d66c37fa66c5e12ad5ffb0
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2025-10-14 21:16:41 -0300

    feat: signin story and removed  default button

--- sdk/ts/IdentityX/src/next/components/SignIn/SignIn.tsx
+++ sdk/ts/IdentityX/src/next/components/SignIn/SignIn.tsx
@@ -1,6 +1,15 @@
-export function SignIn() {
+export interface SignInProps {
+  /** What background color to use */
+  backgroundColor?: string;
+  /** Click Handler - Perform Login */
+  onSubmit: () => void;
+}
+
+export function SignIn({
+  backgroundColor="#dfcdcd"
+}: SignInProps) {
   return (
-    <div className="test">
+    <div className="test" style={{ backgroundColor }}>
       <p>Faça seu Login</p>
     </div>
   );
---
+++ sdk/ts/IdentityX/src/stories/SignIn.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { fn } from 'storybook/internal/test';
+import { SignIn } from '../next';
+
+const meta = {
+  title: "Example/SignIn",
+  component: SignIn,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+  args: { onSubmit: fn() },
+} satisfies Meta<typeof SignIn>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+export const Primary: Story = {
+  args: {
+    
+  }
+};
