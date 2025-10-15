ab3a301f567bd46950cd468b8e655723c0545510
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2025-10-15 20:56:14 -0300

    style: signin

--- sdk/ts/IdentityX/src/next/components/SignIn/SignIn.tsx
+++ sdk/ts/IdentityX/src/next/components/SignIn/SignIn.tsx
@@ -1,16 +1,40 @@
+import BasicInputField from "../Form/BasicInputField";
+import BasicSubmitButton from "../Form/BasicSubmitButton";
+
 export interface SignInProps {
-  /** What background color to use */
-  backgroundColor?: string;
   /** Click Handler - Perform Login */
   onSubmit: () => void;
 }
 
 export function SignIn({
-  backgroundColor="#dfcdcd"
+
 }: SignInProps) {
   return (
-    <div className="test" style={{ backgroundColor }}>
-      <p>Faça seu Login</p>
-    </div>
+    <form className="trieoh trieoh-card trieoh-card--full-rounded">
+      <h3 className="trieoh-card__title">Faça seu Login!</h3>
+      <div className="trieoh-card__fields">
+        <BasicInputField 
+          label="Email" 
+          name="email"
+          placeholder="teste@gmail.com"
+          autoComplete="email"
+          type="email"
+        />
+        <BasicInputField 
+          label="Senha" 
+          name="password"
+          placeholder="**********"
+          autoComplete="current-password"
+          type="password"
+        />
+      </div>
+      <BasicSubmitButton label="Entrar"/>
+      <div className="trieoh-card__divider">
+        <hr />
+        OU
+        <hr />
+      </div>
+      <span className="trieoh-card__other">Ainda não possui uma conta? <span>Cadastra-se</span></span>
+    </form>
   );
 }
