36a289f0a42e511f3d2d38a2318aa751776d419d
MintzyG <ericbraga2000@gmail.com>
2025-10-16 21:11:39 -0300

    Added logout tests, moved login tests to own file

--- IdentityX/tests/goauth_test.go
+++ IdentityX/tests/goauth_test.go
@@ -3,7 +3,6 @@ package testing
 import (
 	"database/sql"
 	"log"
-	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
@@ -86,6 +85,10 @@ func TestGoAuthService(t *testing.T) {
     runLoginTests(t, ctx)
 	})
 
+	t.Run("LogoutTests", func(t *testing.T) {
+    runLogoutTests(t, ctx)
+	})
+
 	t.Logf("rllCtx: %v", ctx)
 }
 
@@ -115,110 +118,10 @@ func runLoginTests(t *testing.T, ctx *rllCtx) {
 	t.Run("LoginSuccess", loginSuccess(ctx))
 }
 
-func loginWrongPassword(user *rllCtx) func(t *testing.T) {
-	return func(t *testing.T) {
-		e := createExpect(t)
-
-		obj := e.POST("/auth/login").
-			WithHeader("Content-Type", "application/json").
-			WithJSON(map[string]interface{}{
-				"email": user.SuccessEmail,
-				"password": "123",
-			}).
-			Expect().
-			Status(http.StatusUnauthorized).
-			JSON().Object()
-
-		obj.Value("module").String().Equal("go-auth-test")
-		obj.Value("message").String().Equal("invalid email or password")
-
-		obj.Value("code").Number().Equal(401)
-	}
+func runLogoutTests(t *testing.T, ctx *rllCtx) {
+	t.Run("LogoutNoTokens", logoutNoTokens())
+	t.Run("LogoutNoRefresh", logoutNoRefresh(ctx))
+	t.Run("LogoutSuccess", logoutSuccess(ctx))
+	t.Run("LoggedOutAlready", loggedOutAlready(ctx))
 }
 
-func loginWrongEmail(user *rllCtx) func(t *testing.T) {
-	return func(t *testing.T) {
-		e := createExpect(t)
-
-		obj := e.POST("/auth/login").
-			WithHeader("Content-Type", "application/json").
-			WithJSON(map[string]interface{}{
-				"email": "wrong@email.com",
-				"password": user.SuccessPasword, 
-			}).
-			Expect().
-			Status(http.StatusUnauthorized).
-			JSON().Object()
-
-		obj.Value("module").String().Equal("go-auth-test")
-		obj.Value("message").String().Equal("invalid email or password")
-
-		obj.Value("code").Number().Equal(401)
-	}
-}
-
-func LoginWrongEmailAndPasword() func(t *testing.T) {
-	return func(t *testing.T) {
-		e := createExpect(t)
-
-		obj := e.POST("/auth/login").
-			WithHeader("Content-Type", "application/json").
-			WithJSON(map[string]interface{}{
-				"email": "wrong@email.com",
-				"password": "Wr0ngP4$$",
-			}).
-			Expect().
-			Status(http.StatusUnauthorized).
-			JSON().Object()
-
-		obj.Value("module").String().Equal("go-auth-test")
-		obj.Value("message").String().Equal("invalid email or password")
-
-		obj.Value("code").Number().Equal(401)
-	}
-}
-
-func loginSuccess(user *rllCtx) func(t *testing.T) {
-	return func(t *testing.T) {
-		e := createExpect(t)
-
-		resp := e.POST("/auth/login").
-			WithHeader("Content-Type", "application/json").
-			WithJSON(map[string]interface{}{
-				"email":    user.SuccessEmail,
-				"password": user.SuccessPasword,
-			}).
-			Expect().
-			Status(http.StatusOK)
-
-		obj := resp.JSON().Object()
-		obj.Value("module").String().Equal("go-auth-test")
-		obj.Value("message").String().Equal("Logged in")
-		obj.Value("code").Number().Equal(200)
-
-		access := resp.Cookie("access_token")
-		if access == nil || access.Raw() == nil {
-			t.Fatalf("expected access_token cookie, got nil")
-		}
-
-		val := access.Value().Raw()
-		if val == "" {
-			t.Fatalf("access_token cookie value is empty")
-		}
-		user.accessToken = val
-
-		refresh := resp.Cookie("refresh_token")
-		if refresh == nil || refresh.Raw() == nil {
-			t.Fatalf("expected refresh_token cookie, got nil")
-		}
-
-		val = refresh.Value().Raw()
-		if val == "" {
-			t.Fatalf("refresh_token cookie value is empty")
-		}
-		user.refreshToken = val
-
-		t.Logf("Access token: %s", user.accessToken)
-		t.Logf("Refresh token: %s", user.refreshToken)
-	}
-}
