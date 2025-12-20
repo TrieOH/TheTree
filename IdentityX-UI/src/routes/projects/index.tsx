563df79611d2656aac7cd406c85dc56d7873951d
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2025-12-20 20:01:39 -0300

    feat: added tabs

--- IdentityX-UI/src/routes/projects/index.tsx
+++ IdentityX-UI/src/routes/projects/index.tsx
@@ -1,3 +1,4 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/projects/')({
@@ -5,5 +6,33 @@ export const Route = createFileRoute('/projects/')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/projects/"!</div>
+  return (
+    <main className="w-full bg-background">
+      <div>
+        <Tabs defaultValue="projects" className='mt-6'>
+          <TabsList className='w-full justify-start gap-4 bg-transparent border-b rounded-none py-0 px-4'>
+            <TabsTrigger
+              value="projects"
+              className='max-w-60 h-full shadow-none! border-b data-[state=active]:border-b-black rounded-none'
+            >
+              Projetos
+            </TabsTrigger>
+            <TabsTrigger
+              value="settings"
+              className='max-w-60 h-full shadow-none! border-b data-[state=active]:border-b-black rounded-none'
+            >
+              Configurações
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent className='p-12' value="projects">What</TabsContent>
+          <TabsContent 
+            className='p-12' 
+            value="settings"
+          >
+            Make changes to your account here.
+          </TabsContent>
+        </Tabs>
+      </div>
+    </main>
+  )
 }
