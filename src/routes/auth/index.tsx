9663387500ae69393929f2916381f3eca4bdd9f4
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2026-01-27 00:25:07 -0300

    added motion and 'feedback' on form submit

--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -1,9 +1,17 @@
 import { requireGuest } from '@/hooks/auth-guard';
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate, useRouter, useSearch } from '@tanstack/react-router'
 import { SignIn, SignUp } from '@trieoh/node-auth-sdk/react'
 import { useState } from 'react'
 
+import { motion } from "motion/react";
+import z from 'zod';
+
+const authSearchSchema = z.object({
+  redirect: z.string().optional().catch(''),
+})
+
 export const Route = createFileRoute('/auth/')({
+  validateSearch: (search) =>authSearchSchema.parse(search),
   beforeLoad: requireGuest,
   staticData: {
     components: {
@@ -17,10 +25,51 @@ export const Route = createFileRoute('/auth/')({
 
 function RouteComponent() {
   const [isLogin, setIsLogin] = useState(true);
+
+  const navigate = useNavigate()
+  const router = useRouter()
+  const search = useSearch({ from: '/auth/' })
+
+  const handleSuccess = async () => {
+    router.update({ 
+      context: { 
+        ...router.options.context, 
+        auth: {...router.options.context.auth!, isAuthenticated: true },
+      },
+    })
+    await router.invalidate()
+
+    const destination = search.redirect || '/projects'
+    await navigate({ to: destination, replace: true })
+  }
+
+  const handleFailure = async (message: string) => {
+    console.error("Auth Failed:", message)
+  }
+
   return (
     <main className='flex justify-center items-center py-2'>
-      {isLogin && <SignIn signUpRedirect={() => setIsLogin(false)} />}
-      {!isLogin && <SignUp loginRedirect={() => setIsLogin(true)} />}
+      <motion.div
+        key={isLogin ? 'signin' : 'signup'}
+        initial={{ opacity: 0, scale: 0.98, y: 5 }}
+        animate={{ opacity: 1, scale: 1, y: 0 }}
+        transition={{ duration: 0.3, ease: "easeOut" }}
+        className="w-full max-w-md"
+      >
+        {isLogin ? (
+          <SignIn 
+            signUpRedirect={() => setIsLogin(false)} 
+            onSuccess={handleSuccess}
+            onFailed={handleFailure}
+          />
+        ) : (
+          <SignUp 
+            loginRedirect={() => setIsLogin(true)} 
+            onSuccess={async () => setIsLogin(true)}
+            onFailed={handleFailure}
+          />
+        )}
+      </motion.div>
     </main>
   )
 }
