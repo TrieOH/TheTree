a47fcccc622119d9924f1ebe0c39d19e1a816853
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2026-02-03 22:00:34 -0300

    added variants to shadowbutton

--- src/shared/ui/buttons/ShadowButton.tsx
+++ src/shared/ui/buttons/ShadowButton.tsx
@@ -1,6 +1,23 @@
 import { cn } from "@/shared/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
 
-interface PropsI {
+const buttonVariants = cva(
+  "flex cursor-pointer gap-1 font-extralight border rounded-sm p-2 items-center md:text-sm text-xs transition-all duration-300 ease-out",
+  {
+    variants: {
+      variant: {
+        default: "bg-primary-foreground text-card-foreground border-primary shadow-[1px_1px_0_0_var(--color-primary)] hover:shadow-[2px_2px_0_0_var(--color-primary)]",
+        destructive: "bg-destructive text-destructive-foreground border-destructive shadow-[1px_1px_0_0_var(--color-destructive)] hover:shadow-[2px_2px_0_0_var(--color-destructive)]",
+        accent: "text-primary-foreground bg-primary border-accent shadow-[1px_1px_0_0_var(--color-accent)] hover:shadow-[2px_2px_0_0_var(--color-accent)]",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+interface PropsI extends VariantProps<typeof buttonVariants> {
   value?: string;
   leftIcon?: React.ReactNode;
   type?: "button" | "submit";
@@ -9,27 +26,16 @@ interface PropsI {
   disabled?: boolean;
 }
 
-export function ShadowButton({ value, leftIcon, className, onClick, type = "button", disabled }: PropsI) {
+export function ShadowButton({ value, leftIcon, className, onClick, type = "button", disabled, variant }: PropsI) {
   return (
     <button
       type={type}
       className={cn(
-        "flex cursor-pointer gap-1 font-extralight bg-primary-foreground text-card-foreground",
-        "border border-primary rounded-sm p-2 items-center md:text-sm text-xs ",
-
-        // base state
-        "shadow-[1px_1px_0_0_var(--color-primary)]",
-
-        // hover
-        "hover:shadow-[2px_2px_0_0_var(--color-primary)]",
-
+        buttonVariants({ variant, className }),
         // pressed
         "active:translate-x-px active:translate-y-px",
         "active:shadow-none",
-
-        "transition-all duration-300 ease-out",
         disabled && "opacity-50 cursor-not-allowed shadow-none hover:shadow-none active:translate-x-0 active:translate-y-0",
-        className
       )}
       onClick={onClick}
       disabled={disabled}
