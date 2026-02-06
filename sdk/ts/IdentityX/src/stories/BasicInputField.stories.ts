e975ea2bd1083eedc7d6a9d1d96757f2c09fee5c
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2026-02-06 18:22:49 -0300

    changed version and added onBlur on inputfield

--- sdk/ts/IdentityX/src/stories/BasicInputField.stories.ts
+++ sdk/ts/IdentityX/src/stories/BasicInputField.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
-import BasicInputField from '../react/components/Form/BasicInputField';
+import { BasicInputField } from '../react';
 
 const meta = {
   title: "Example/BasicInputField",
