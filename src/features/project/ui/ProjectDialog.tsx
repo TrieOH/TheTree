a039f6c23216403b2fe78411819e10b2b83951f5
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2026-02-08 15:14:06 -0300

    feat: project update

--- src/features/project/ui/ProjectDialog.tsx
+++ src/features/project/ui/ProjectDialog.tsx
@@ -5,12 +5,14 @@ import { formOptions } from "@tanstack/react-form";
 import type { FieldConfig } from "@/shared/ui/form/types";
 import { useCrudOperations } from "@/shared/lib/hooks/useCrudStore";
 import { projectCRUDSchema, type ProjectCRUD } from "../model/types";
-import { createProjectFn } from "../api";
+import { createProjectFn, patchProjectFn } from "../api";
 import { toast } from "sonner";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useStore } from "@tanstack/react-store";
 
 export function ProjectDialog() {
   const queryClient = useQueryClient();
+  const { formData, mode } = useStore(projectStore, (state) => state);
 
   const createProjectMutation = useMutation({
     mutationFn: createProjectFn,
@@ -25,18 +27,34 @@ export function ProjectDialog() {
     },
   });
 
+  const patchProjectMutation = useMutation({
+    mutationFn: patchProjectFn,
+    onSuccess: (response) => {
+      if (response.success) {
+        toast.success(response.message || "Updated project");
+        queryClient.invalidateQueries({ queryKey: ["projects"] });
+      } else toast.error(`Failed to update project: ${response.message}`);
+    },
+    onError: (error) => {
+      toast.error(`Failed to update project: ${error.message}`);
+    },
+  });
+
   const { handleSubmit } = useCrudOperations({
     store: projectStore,
     autoClose: true,
     onCreate: async (data) => {
       createProjectMutation.mutate(data);
     },
+    onUpdate: async (id, data) => {
+      patchProjectMutation.mutate({ id, ...data } as ProjectCRUD);
+    },
   });
   const fields: FieldConfig[] = [
     {name: "project_name", label: "Project Name", placeholder: "My Awesome Project", autoComplete: "project_name"}
   ]
   const projectOpts = formOptions({
-    defaultValues: { id: "", project_name: "" } as ProjectCRUD,
+    defaultValues: (mode === 'create' ? { id: "", project_name: "" } : formData) as ProjectCRUD,
     validators: {
       onChange: projectCRUDSchema,
       onMount: projectCRUDSchema,
