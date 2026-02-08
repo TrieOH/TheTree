2503d1d6b3612bf84888085ef0900fab960340e1
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2026-02-07 21:56:13 -0300

    now the cards pick the real info from projects

--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -2,9 +2,8 @@ import { createFileRoute } from '@tanstack/react-router'
 import { requireAuth } from '@/features/auth/lib/route-guard';
 import { ProjectDialog } from '@/features/project/ui/ProjectDialog';
 import ProjectCard from '@/features/project/ui/ProjectCard';
-// import { createServerFn } from '@tanstack/react-start';
-
-// createServerFn
+import { projectsQueryOptions } from '@/features/project/api';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
 export const Route = createFileRoute('/projects/')({
   beforeLoad: requireAuth,
@@ -13,31 +12,29 @@ export const Route = createFileRoute('/projects/')({
       header: "projects"
     }
   },
+  loader: async ({ context: { queryClient }}) => {
+    await queryClient.ensureQueryData(projectsQueryOptions)
+    return {}
+  },
   component: RouteComponent,
-  // loader
 })
 
 function RouteComponent() {
+  const projectsQuery = useSuspenseQuery(projectsQueryOptions)
+  const projects = projectsQuery.data
+  
   return (
-    <main className="w-full bg-background flex flex-col items-center mt-4">
+    <main className="w-full bg-background flex flex-col items-center my-4">
       <div className="text-center space-y-1 mb-7">
         <h1 className="font-bold text-3xl">Your Projects</h1>
         <p className="font-extralight text-sm">
           Manage your projects configurations
         </p>
       </div>
-      <div className="max-w-7xl">
-        <ProjectCard
-          data={{
-            id: "0",
-            is_active: true,
-            project_name: "Univents",
-            metadata: {},
-            created_at: "2026-02-06T23:06:11.608471Z",
-            updated_at: "2026-02-07T23:06:11.608471Z",
-            owner_id: ""
-          }}
-        />
+      <div className="max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} data={project} />
+        ))}
       </div>
       <ProjectDialog />
     </main>
