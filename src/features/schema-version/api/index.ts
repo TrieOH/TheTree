b0718aecb3660d21cbc4f7006e0475a0f341fff4
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2026-02-15 21:57:45 -0300

    added more actions

--- src/features/schema-version/api/index.ts
+++ src/features/schema-version/api/index.ts
@@ -1,7 +1,7 @@
 import { authFetcher, tanstackQueryFetcher } from "@/shared/lib/api/fetch";
 import { createClientOnlyFn } from "@tanstack/react-start";
 import { queryOptions } from "@tanstack/react-query";
-import type { DetailedSchemaVersion, SchemaVersion, SchemaVersionFields, VersionDraft } from "../model/types";
+import type { DetailedSchemaVersion, PartialVersionField, SchemaFieldOption, SchemaFieldRequiredRule, SchemaFieldVisibilityRule, SchemaVersion, SchemaVersionFields, VersionDraft } from "../model/types";
 
 export const createSchemaVersionDraftFn = createClientOnlyFn((versionData: VersionDraft) => {
   const { project_id, schema_id } = versionData
@@ -19,16 +19,6 @@ export const publishSchemaVersionFn = createClientOnlyFn((fieldsData: VersionDra
     headers: { "Content-Type": "application/json" },
   });
 });
-
-export const publishSchemaVersionFieldFn = createClientOnlyFn((fieldsData: SchemaVersionFields) => {
-  const { project_id, schema_id, version, fields } = fieldsData
-  return authFetcher<string>(`/projects/${project_id}/schemas/${schema_id}/v${version}`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" }, // it's already used in the lib per default
-    body: JSON.stringify({fields})
-  });
-});
-
 
 export const getLatestSchemaVersionFn = createClientOnlyFn(async ({
   queryKey,
@@ -68,6 +58,8 @@ export const currentSchemaVersionQueryOptions = (project_id: string, schema_id: 
   })
 }
 
+// Fields
+
 export const getSchemaVersionByIdFn = createClientOnlyFn(async ({
   queryKey,
 }: {
@@ -86,3 +78,86 @@ export const schemaVersionByIdQueryOptions = (project_id: string, schema_id: str
     enabled: Boolean(project_id && schema_id && version !== undefined)
   })
 }
+
+
+
+export const createSchemaVersionFieldFn = createClientOnlyFn((fieldsData: SchemaVersionFields) => {
+  const { project_id, schema_id, version, fields } = fieldsData
+  return authFetcher<string>(`/projects/${project_id}/schemas/${schema_id}/v${version}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" }, // it's already used in the lib per default
+    body: JSON.stringify({fields})
+  });
+});
+
+export const deleteSchemaVersionFieldFn = createClientOnlyFn(
+  ({ project_id, schema_id, version, field_id }: { project_id: string; schema_id: string; version: number; field_id: string }) => {
+    return authFetcher<string>(`/projects/${project_id}/schemas/${schema_id}/v${version}/fields/${field_id}`, {
+      method: "DELETE",
+    });
+  }
+);
+
+export const updateSchemaVersionFieldFn = createClientOnlyFn(
+  ({ project_id, schema_id, version, field_id, fieldData }: { project_id: string; schema_id: string; version: number; field_id: string; fieldData: PartialVersionField }) => {
+    return authFetcher<string>(`/projects/${project_id}/schemas/${schema_id}/v${version}/fields/${field_id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(fieldData),
+    });
+  }
+);
+
+export const setSchemaFieldOptionsFn = createClientOnlyFn(
+  ({ project_id, schema_id, version, field_id, options }: { project_id: string; schema_id: string; version: number; field_id: string; options: SchemaFieldOption[] }) => {
+    return authFetcher<string>(`/projects/${project_id}/schemas/${schema_id}/v${version}/fields/${field_id}/options`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(options),
+    });
+  }
+);
+
+export const deleteSchemaFieldOptionFn = createClientOnlyFn(
+  ({ project_id, schema_id, version, field_id, option_id }: { project_id: string; schema_id: string; version: number; field_id: string; option_id: string }) => {
+    return authFetcher<string>(`/projects/${project_id}/schemas/${schema_id}/v${version}/fields/${field_id}/options/${option_id}`, {
+      method: "DELETE",
+    });
+  }
+);
+
+export const setSchemaFieldVisibilityRulesFn = createClientOnlyFn(
+  ({ project_id, schema_id, version, field_id, rules }: { project_id: string; schema_id: string; version: number; field_id: string; rules: SchemaFieldVisibilityRule[] }) => {
+    return authFetcher<string>(`/projects/${project_id}/schemas/${schema_id}/v${version}/fields/${field_id}/visibility-rules`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(rules),
+    });
+  }
+);
+
+export const deleteSchemaFieldVisibilityRuleFn = createClientOnlyFn(
+  ({ project_id, schema_id, version, field_id, rule_id }: { project_id: string; schema_id: string; version: number; field_id: string; rule_id: string }) => {
+    return authFetcher<string>(`/projects/${project_id}/schemas/${schema_id}/v${version}/fields/${field_id}/visibility-rules/${rule_id}`, {
+      method: "DELETE",
+    });
+  }
+);
+
+export const setSchemaFieldRequiredRulesFn = createClientOnlyFn(
+  ({ project_id, schema_id, version, field_id, rules }: { project_id: string; schema_id: string; version: number; field_id: string; rules: SchemaFieldRequiredRule[] }) => {
+    return authFetcher<string>(`/projects/${project_id}/schemas/${schema_id}/v${version}/fields/${field_id}/required-rules`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(rules),
+    });
+  }
+);
+
+export const deleteSchemaFieldRequiredRuleFn = createClientOnlyFn(
+  ({ project_id, schema_id, version, field_id, rule_id }: { project_id: string; schema_id: string; version: number; field_id: string; rule_id: string }) => {
+    return authFetcher<string>(`/projects/${project_id}/schemas/${schema_id}/v${version}/fields/${field_id}/required-rules/${rule_id}`, {
+      method: "DELETE",
+    });
+  }
+);
