938e11ecaef9fe1f14099ec6cfa28446c338d4bf
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2026-02-16 23:05:57 -0300

    requiresAuth validation on schemas/editor

--- src/routes/schemas/editor/index.tsx
+++ src/routes/schemas/editor/index.tsx
@@ -1,7 +1,17 @@
+import { requireAuth } from '@/features/auth/lib/route-guard';
+import { navigationStore } from '@/features/navigation';
 import FieldEditor from '@/features/schema-version/ui/FieldEditor'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/schemas/editor/')({
+  beforeLoad: async (ctx) => {
+    requireAuth(ctx)
+    const { currentProjectId, currentSchemaId } = navigationStore.state;
+    if (typeof window !== 'undefined') {
+      if(!currentProjectId) throw redirect({ to: '/projects' });
+      if(!currentSchemaId) throw redirect({ to: '/projects/config' });
+    }
+  },
   component: RouteComponent,
   staticData: {components: {header: "schemas/editor"}}
 })
