996c9aaeedcd2860fb76912979ec11010295a590
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2026-02-17 23:17:20 -0300

    sing in forgot password redirect and more examples

--- sdk/ts/IdentityX/src/stories/components/SignInWithProvider.tsx
+++ sdk/ts/IdentityX/src/stories/components/SignInWithProvider.tsx
@@ -1,9 +1,18 @@
 import { AuthProvider, SignIn } from "../../react";
+import { type MouseEvent } from "react";
 
-export default function SignInWithProvider() {
+interface SignInWithProviderProps {
+  signUpRedirect?:(e: MouseEvent<HTMLSpanElement>) => void;
+  forgotPasswordRedirect?: (e: MouseEvent<HTMLSpanElement>) => void;
+}
+
+export default function SignInWithProvider({ forgotPasswordRedirect, signUpRedirect }: SignInWithProviderProps) {
   return (
     <AuthProvider baseURL="http://localhost:8080">
-      <SignIn />
+      <SignIn 
+        forgotPasswordRedirect={forgotPasswordRedirect} 
+        signUpRedirect={signUpRedirect}
+      />
     </AuthProvider>
   )
 }
