51e9d01e2cd2e676f1495e7a8c204fea1c128f46
SoraAsc <57465032+SoraAsc@users.noreply.github.com>
2026-02-27 14:40:15 -0300

    feat: permission tree

--- IdentityX-UI/src/features/user/lib/node-tree-utils.ts
+++ IdentityX-UI/src/features/user/lib/node-tree-utils.ts
@@ -1,3 +1,4 @@
+import type { Permission } from "@/features/permission/model/types";
 import type { Node, RoleWithPermissions } from "../model/types";
 
 export const buildRolePermissionsToNodeTree = (roleWithPermissions: RoleWithPermissions[]) => {
@@ -39,6 +40,40 @@ export const buildRolePermissionsToNodeTree = (roleWithPermissions: RoleWithPerm
     });
 
     nodeTree.children?.push(roleNode);
+  });
+
+  return nodeTree;
+}
+
+export const buildDirectPermissionsToNodeTree = (permissions: Permission[]) => {
+  const nodeTree: Node = {
+    id: 'root',
+    name: {
+      receiverName: "User",
+      applicationName: "Scope"
+    },
+    children: []
+  };
+
+  const permissionsByObject: Record<string, Node> = {};
+
+  permissions.forEach(permission => {
+    if (!permissionsByObject[permission.object]) {
+      permissionsByObject[permission.object] = {
+        id: `direct-${permission.object}`,
+        name: permission.object,
+        children: []
+      };
+    }
+
+    permissionsByObject[permission.object].children?.push({
+      id: `direct-${permission.id}`,
+      name: permission.action,
+    });
+  });
+
+  Object.values(permissionsByObject).forEach(objectNode => {
+    nodeTree.children?.push(objectNode);
   });
 
   return nodeTree;
