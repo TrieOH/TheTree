3a71b915b2ef395ab640d41a00c0b1e9aad0830f
MintzyG <ericbraga2000@gmail.com>
2026-03-04 21:23:28 -0300

    added meta to scope creation

--- IdentityX-SDK/scopes.go
+++ IdentityX-SDK/scopes.go
@@ -2,6 +2,7 @@ package goauth
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -9,28 +10,32 @@ import (
 )
 
 type Scope struct {
-	ID         uuid.UUID  `json:"id"`
-	ParentID   *uuid.UUID `json:"parent_id"`
-	ProjectID  *uuid.UUID `json:"project_id"`
-	ExternalID *string    `json:"external_id"`
-	Type       string     `json:"type"`
-	Name       *string    `json:"name"`
-	CreatedAt  time.Time  `json:"created_at"`
+	ID         uuid.UUID        `json:"id"`
+	ParentID   *uuid.UUID       `json:"parent_id"`
+	ProjectID  *uuid.UUID       `json:"project_id"`
+	ExternalID *string          `json:"external_id"`
+	Type       string           `json:"type"`
+	Name       *string          `json:"name"`
+	Meta       *json.RawMessage `json:"meta"`
+	CreatedAt  time.Time        `json:"created_at"`
 }
 
 type ScopeService struct {
 	client *Client
 }
 
-func (s *ScopeService) Create(ctx context.Context, name string, externalID *string) (*Scope, error) {
-	return s.CreateWithParent(ctx, name, externalID, nil)
+func (s *ScopeService) Create(ctx context.Context, name string, externalID *string, meta ...json.RawMessage) (*Scope, error) {
+	return s.CreateWithParent(ctx, name, externalID, nil, meta...)
 }
 
-func (s *ScopeService) CreateWithParent(ctx context.Context, name string, externalID *string, parentID *uuid.UUID) (*Scope, error) {
+func (s *ScopeService) CreateWithParent(ctx context.Context, name string, externalID *string, parentID *uuid.UUID, meta ...json.RawMessage) (*Scope, error) {
 	reqBody := map[string]any{
 		"name":        name,
 		"external_id": externalID,
 		"parent_id":   parentID,
+	}
+	if len(meta) > 0 && meta[0] != nil {
+		reqBody["meta"] = meta[0]
 	}
 	path := fmt.Sprintf("/projects/%s/scopes", s.client.projectID)
 	req, err := s.client.newRequest(ctx, "POST", path, reqBody)
