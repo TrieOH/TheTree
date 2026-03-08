8013b3e43a67ed3b6e4fb657d7a99aad633f3b45
SoraAsc <57465032+SoraAsc@users.noreply.github.com>
2026-03-08 01:13:29 -0300

    feat: products model

---
+++ Univents-UI/src/features/products/api/index.ts
@@ -0,0 +1,66 @@
+import { createClientOnlyFn } from "@tanstack/react-start";
+import { queryOptions } from "@tanstack/react-query";
+import { authFetcher, tanstackQueryFetcher } from "@/shared/lib/api/fetch";
+import type { ProductCreateI, ProductI } from "../model";
+
+/**
+ * Creates a new Product on the server.
+ * @param productData - The data for the new product.
+ * @returns A promise that resolves to the API response containing the newly created product.
+ */
+export const createProductFn = createClientOnlyFn((
+  productData: ProductCreateI, eventId: string, editionId: string
+) => {
+  return authFetcher.post<ProductI>(
+    `/events/${eventId}/editions/${editionId}/products`,
+    productData
+  );
+});
+
+
+/**
+ * Fetches all products for a specific edition from the server.
+ * @returns A promise that resolves to an array of Product objects.
+ */
+export const getAllProductsFn = createClientOnlyFn(async (eventId: string, editionId: string) => {
+  try {
+    return await tanstackQueryFetcher<ProductI[]>(`/events/${eventId}/editions/${editionId}/products`);
+  } catch {
+    return [];
+  }
+});
+
+/**
+ * Query options for fetching all products for a specific edition, using TanStack Query.
+ * @returns An object containing the query key and query function for fetching all products for a specific edition.
+ */
+export const allProductsQueryOptions = (eventId: string, editionId: string) => {
+  return queryOptions({
+    queryKey: ['allProducts', eventId, editionId],
+    queryFn: () => getAllProductsFn(eventId, editionId),
+  })
+}
+
+
+/**
+ * Fetches all admin products for a specific edition from the server.
+ * @returns A promise that resolves to an array of Product objects.
+ */
+export const getAllAdminProductsFn = createClientOnlyFn(async (eventId: string, editionId: string) => {
+  try {
+    return await tanstackQueryFetcher<ProductI[]>(`/events/${eventId}/editions/${editionId}/products/admin`);
+  } catch {
+    return [];
+  }
+});
+
+/**
+ * Query options for fetching all admin products for a specific edition, using TanStack Query.
+ * @returns An object containing the query key and query function for fetching all admin products for a specific edition.
+ */
+export const allAdminProductsQueryOptions = (eventId: string, editionId: string) => {
+  return queryOptions({
+    queryKey: ['allAdminProducts', eventId, editionId],
+    queryFn: () => getAllAdminProductsFn(eventId, editionId),
+  })
+};
---
+++ Univents-UI/src/features/products/model/index.ts
@@ -0,0 +1,46 @@
+import z from "zod";
+
+const productTypeSchema = z
+  .enum(
+    ["merchandise", "ticket", "token", "bundle"],
+    { error: "Invalid product type" }
+  ).default("merchandise");
+
+type ProductType = z.infer<typeof productTypeSchema>
+
+export const productCreateSchema = z.object({
+  edition_scope_id: z.uuid(),
+  name: z.string().min(3),
+  description: z.string().optional().nullable(),
+  type: productTypeSchema,
+  ticket_id: z.uuid().optional().nullable(),
+  price_cents: z.int().nonnegative(),
+  available_from: z.iso.datetime().optional().nullable(),
+  available_until: z.iso.datetime().optional().nullable(),
+  has_inventory: z.boolean().default(false),
+  inventory_quantity: z.int().nonnegative().default(0),
+})
+
+export type ProductCreateI = z.infer<typeof productCreateSchema>
+
+
+export interface ProductI {
+  id: string;
+  scope_id: string;
+  edition_id: string;
+  name: string;
+  description: string | null;
+  type: ProductType;
+  ticket_id: string | null;
+  price_cents: number;
+  status: "draft" | "available" | "sold_out" | "unavailable";
+  available_from: string | null;
+  available_until: string | null;
+  has_inventory: boolean;
+  inventory_quantity: number;
+  inventory_remaining: number;
+  created_by: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string | null;
+}
