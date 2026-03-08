7fd5f574939eb255488e5a715802f0c201703eb8
MintzyG <ericbraga2000@gmail.com>
2026-03-08 19:03:33 -0300

    disconnect provider

---
+++ Payssage-SDK/credentials.go
@@ -0,0 +1,14 @@
+package paymentsSDK
+
+import (
+	"context"
+	"fmt"
+)
+
+func (c *Client) DisconnectProvider(ctx context.Context, workspaceName, credentialID string) (*ProviderCredential, error) {
+	var out ProviderCredential
+	if err := c.do(ctx, "DELETE", fmt.Sprintf("/workspaces/%s/providers/%s/disconnect", workspaceName, credentialID), nil, &out); err != nil {
+		return nil, err
+	}
+	return &out, nil
+}
