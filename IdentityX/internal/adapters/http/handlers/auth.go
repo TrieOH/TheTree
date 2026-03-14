23f925efc4af0632cb09c6065a6b090b0978975b
MintzyG <ericbraga2000@gmail.com>
2026-03-14 03:47:45 -0300

    returning tokens in response body

--- IdentityX/internal/adapters/http/handlers/auth.go
+++ IdentityX/internal/adapters/http/handlers/auth.go
@@ -90,14 +90,10 @@ func (handler *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	accessCookie := CreateCookie("access_token", tokens.AccessTokenString, tokens.Domain, tokens.AccessExpiresAt)
-	refreshCookie := CreateCookie("refresh_token", tokens.RefreshTokenString, tokens.Domain, tokens.RefreshExpiresAt)
-
-	http.SetCookie(w, accessCookie)
-	http.SetCookie(w, refreshCookie)
-
 	resp.OK("Logged in").WithData(map[string]any{
 		"is_up_to_date": tokens.IsUpToDate,
+		"access_token":  tokens.AccessTokenString,
+		"refresh_token": tokens.RefreshTokenString,
 	}).Send(w)
 }
 
@@ -313,14 +309,10 @@ func (handler *AuthHandler) ProjectLogin(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	accessCookie := CreateCookie("access_token", tokens.AccessTokenString, tokens.Domain, tokens.AccessExpiresAt)
-	refreshCookie := CreateCookie("refresh_token", tokens.RefreshTokenString, tokens.Domain, tokens.RefreshExpiresAt)
-
-	http.SetCookie(w, accessCookie)
-	http.SetCookie(w, refreshCookie)
-
 	resp.OK("Logged in").WithData(map[string]any{
 		"is_up_to_date": tokens.IsUpToDate,
+		"access_token":  tokens.AccessTokenString,
+		"refresh_token": tokens.RefreshTokenString,
 	}).Send(w)
 }
 
