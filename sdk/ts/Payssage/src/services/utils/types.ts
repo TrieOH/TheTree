87574d24fdd50e1f655a342407b7566fc2307587
SoraAsc <57465032+SoraAsc@users.noreply.github.com>
2026-03-18 17:59:47 -0300

    feat: added get all workspace intent

--- sdk/ts/Payssage/src/services/utils/types.ts
+++ sdk/ts/Payssage/src/services/utils/types.ts
@@ -2,3 +2,25 @@ export interface ConnectRequestI {
   final_redirect_url: string;
   provider_redirect_url: string;
 }
+
+export interface IntentResponseI {
+  id: string;
+  workspace_id: string;
+  amount: number;
+  currency: string;
+  status: string;
+  client_secret: string;
+  provider: string;
+  provider_payment_id: string | null;
+  metadata: RawJsonValue;
+  created_at: string;
+  updated_at: string;
+}
+
+type RawJsonValue =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: RawJsonValue }
+  | RawJsonValue[];
