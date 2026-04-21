bdab1dcd03353866e3889eb2bcf7965ce0278968
SoraAsc <57465032+SoraAsc@users.noreply.github.com>
2026-04-21 17:34:41 -0300

    renamed to IdentityX

--- IdentityX-UI/src/routes/__root.tsx
+++ IdentityX-UI/src/routes/__root.tsx
@@ -22,7 +22,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
     meta: [
       { charSet: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { title: 'TrieAuth' },
+      { title: 'IdentityX' },
     ],
     links: [{ rel: 'stylesheet', href: appCss }],
   }),
