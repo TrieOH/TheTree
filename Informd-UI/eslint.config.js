a2eecfdeb392c7d29e8eeb305bd5b80f721c818a
SoraAsc <57465032+SoraAsc@users.noreply.github.com>
2026-05-09 01:07:59 -0300

    added react compiler

--- Informd-UI/eslint.config.js
+++ Informd-UI/eslint.config.js
@@ -1,11 +1,16 @@
 //  @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
+import reactCompiler from 'eslint-plugin-react-compiler'
 
 export default [
   ...tanstackConfig,
   {
+    plugins: {
+      'react-compiler': reactCompiler,
+    },
     rules: {
+      'react-compiler/react-compiler': 'error',
       'import/no-cycle': 'off',
       'import/order': 'off',
       'sort-imports': 'off',
