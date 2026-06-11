8d976c7c4efc92f2c2bd15f2748dff893c724338
SoraAsc <57465032+SoraAsc@users.noreply.github.com>
2026-06-10 23:20:00 -0300

    returned to session storage :)

--- sdk/ts/identityx/src/store/token-store.ts
+++ sdk/ts/identityx/src/store/token-store.ts
@@ -6,10 +6,10 @@ const ACCESS_TOKEN_KEY = "trieoh_access_token";
 export const tokenStore = {
   getAccessToken: () => deobfuscate<string>(sessionBrowserStorage.getItem(ACCESS_TOKEN_KEY)),
   setAccessToken: (token: string | null) => {
-    if (token) localStorage.setItem(ACCESS_TOKEN_KEY, obfuscate(token));
-    else localStorage.removeItem(ACCESS_TOKEN_KEY);
+    if (token) sessionBrowserStorage.setItem(ACCESS_TOKEN_KEY, obfuscate(token));
+    else sessionBrowserStorage.removeItem(ACCESS_TOKEN_KEY);
   },
   clear: () => {
-    localStorage.removeItem(ACCESS_TOKEN_KEY);
+    sessionBrowserStorage.removeItem(ACCESS_TOKEN_KEY);
   }
 };
