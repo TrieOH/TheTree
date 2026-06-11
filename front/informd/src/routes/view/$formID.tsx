47f38759cc3fc54095df09e7b7ab2cffdfa1fc37
SoraAsc <57465032+SoraAsc@users.noreply.github.com>
2026-06-10 22:53:24 -0300

    (informd-ui) updated identityx-sdk version

--- front/informd/src/routes/view/$formID.tsx
+++ front/informd/src/routes/view/$formID.tsx
@@ -1,4 +1,3 @@
-import { requireAuth } from '#/features/auths/lib/route-guard';
 import FormContainer from '#/features/submissions/ui/form-view/form-container';
 import { createFileRoute } from '@tanstack/react-router'
 import { z } from 'zod'
@@ -9,7 +8,6 @@ const searchSchema = z.object({
 
 export const Route = createFileRoute('/view/$formID')({
   validateSearch: (search) => searchSchema.parse(search),
-  beforeLoad: requireAuth,
   component: RouteComponent,
 })
 
