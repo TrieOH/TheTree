336d3680d8a6cf3e70e008edb1d8b637cfc78e90
MintzyG <ericbraga2000@gmail.com>
2026-07-09 01:59:46 -0300

    feat(univents): activities and editions now return template id

--- api/univents/internal/features/certifications/handlers/get_template.go
+++ api/univents/internal/features/certifications/handlers/get_template.go
@@ -8,15 +8,11 @@ import (
 
 func (h *Handlers) GetTemplateByID(w http.ResponseWriter, r *http.Request) {
 	req := fun.From(r)
-	editionID, err := req.Path("edition_id").UUID()
-	if fun.Bail(w, err) {
-		return
-	}
 	templateID, err := req.Path("template_id").UUID()
 	if fun.Bail(w, err) {
 		return
 	}
-	tpl, err := h.queries.GetTemplateByID(r.Context(), templateID, editionID)
+	tpl, err := h.queries.GetTemplateByID(r.Context(), templateID)
 	if fun.Bail(w, err) {
 		return
 	}
