f9df4d69b5ee79eef0ba3201fc341c5b78ab72d8
MintzyG <ericbraga2000@gmail.com>
2026-07-09 21:18:48 -0300

    feat(identityx): added profile relate queries

--- api/identityx/internal/features/profile_schemas/profile_schemas.go
+++ api/identityx/internal/features/profile_schemas/profile_schemas.go
@@ -1,7 +1,11 @@
 package profile_schemas
 
 import (
+	"IdentityX/internal/features/profile_schemas/queries"
 	"IdentityX/internal/features/profile_schemas/repos"
 )
 
 var NewRepo = repos.NewSchemaRepo
+var NewQueries = queries.New
+
+type Queries = queries.Queries
--- api/identityx/internal/features/profiles/profiles.go
+++ api/identityx/internal/features/profiles/profiles.go
@@ -1,7 +1,11 @@
 package profiles
 
 import (
+	"IdentityX/internal/features/profiles/queries"
 	"IdentityX/internal/features/profiles/repos"
 )
 
 var NewRepo = repos.NewProfileRepo
+var NewQueries = queries.New
+
+type Queries = queries.Queries
