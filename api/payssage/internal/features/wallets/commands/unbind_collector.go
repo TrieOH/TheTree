317a56f0b957c6faee5323356af1f72ec7165775
MintzyG <ericbraga2000@gmail.com>
2026-07-17 15:07:52 -0300

    feat(payssage): bind and unbind wallet collector

---
+++ api/payssage/internal/features/wallets/commands/unbind_collector.go
@@ -0,0 +1,39 @@
+package commands
+
+import (
+	"context"
+	"payssage/models"
+	idx "sdk/identityx"
+
+	"github.com/MintzyG/fun"
+	"github.com/google/uuid"
+)
+
+func (c *Commands) UnbindCollector(ctx context.Context, walletID uuid.UUID) error {
+	ctx, span := c.tracer.Start(ctx, "UnbindCollector")
+	defer span.End()
+
+	ident, err := idx.RequireIdentity(ctx)
+	if err != nil {
+		return err
+	}
+
+	wallet, err := c.wallets.GetByID(ctx, walletID)
+	if err != nil {
+		return err
+	}
+
+	if wallet.OrganizationID != nil {
+		org, err := c.orgs.GetByID(ctx, *wallet.OrganizationID)
+		if err != nil {
+			return err
+		}
+		if err := c.checkRole(ctx, org, ident.Sub.ID, models.OrganizationRoleAdmin); err != nil {
+			return err
+		}
+	} else if wallet.OwnerID != ident.Sub.ID {
+		return fun.ErrForbidden("insufficient permissions")
+	}
+
+	return c.wallets.UnbindCollector(ctx, walletID)
+}
