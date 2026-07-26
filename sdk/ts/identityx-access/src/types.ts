e0596b19d1005c691fa6f2cd60c663578f4bd153
SoraAsc <57465032+SoraAsc@users.noreply.github.com>
2026-07-26 00:44:40 -0300

    fix: update identityx-access-sdk-ts to version 0.1.5 and refactor types

--- sdk/ts/identityx-access/src/types.ts
+++ sdk/ts/identityx-access/src/types.ts
@@ -1,29 +1,69 @@
-import type {
-  Actor,
-  ApiKey,
-  Capability,
-  CreateActorRequest,
-  CreateApiKeyRequest,
-  CreateApiKeyResponse,
-  CreateCapabilityRequest,
-} from "@trieoh/identityx-models";
+export interface ApiKey {
+  id: string;
+  subject_id: string;
+  name: string;
+  display_prefix: string;
+  key_hash: string;
+  metadata: unknown;
+  expires_at?: string;
+  revoked_at?: string;
+  last_used_at?: string;
+  created_by: string;
+  created_at: string;
+}
 
-export type {
-  Actor,
-  ApiKey,
-  Capability,
-  CreateActorRequest,
-  CreateApiKeyRequest,
-  CreateApiKeyResponse,
-  CreateCapabilityRequest,
-};
+export interface Actor {
+  id: string;
+  project_id?: string;
+  auth_method: string;
+  verified_at?: string;
+  email?: string;
+  type: string;
+  metadata?: unknown;
+  created_at: string;
+  updated_at: string;
+  deleted_at?: string;
+}
+
+export interface Capability {
+  id: string;
+  project_id?: string;
+  resource: string;
+  action: string;
+  created_by: string;
+  created_at: string;
+}
+
+export interface CreateApiKeyRequest {
+  subject_id?: string;
+  capabilities: string[];
+  name: string;
+  env: string;
+  expires_at?: string;
+}
+
+export interface CreateApiKeyResponse {
+  key?: ApiKey;
+  raw_key: string;
+}
+
+export interface CreateCapabilityRequest {
+  resource: string;
+  action: string;
+}
+
+export interface CreateActorRequest {
+  auth_method: string;
+  type: string;
+  email?: string;
+}
 
 export interface IdentityXAccessClientConfig {
   baseURL: string;
   apiKey: string;
 }
 
-export interface CreateIdentityXAccessClientConfig extends IdentityXAccessClientConfig { }
+export interface CreateIdentityXAccessClientConfig extends IdentityXAccessClientConfig {}
 
 export interface ApiKeyCreateResponseI extends CreateApiKeyResponse { }
 export interface ApiKeyI extends ApiKey { }
