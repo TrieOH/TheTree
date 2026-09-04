6ee1b3b9c10f0fda550131f9f5417be0d15907b7
SoraAsc <57465032+SoraAsc@users.noreply.github.com>
2026-09-03 22:39:21 -0300

    feat(univents-ui): update defaultPreloadStaleTime to 30 seconds

--- front/univents/src/router.tsx
+++ front/univents/src/router.tsx
@@ -22,7 +22,7 @@ export function getRouter() {
 
     scrollRestoration: true,
     defaultPreload: "intent",
-    defaultPreloadStaleTime: 0,
+    defaultPreloadStaleTime: 30_000,
   });
 
   setupRouterSsrQueryIntegration({ router, queryClient: context.queryClient });
